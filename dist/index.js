700cfebd52a56522dc508f19b7ade1c4cd9b716c
Michel Novellino <michelnovellino.programador@gmail.com>
2019-10-14 22:04:42 -0400

    configuracion inicial

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var express = require('express');
+
+var app = express();
+app.listen(3001, function () {
+  return console.log('server runing');
+});
+//# sourceMappingURL=index.js.map
